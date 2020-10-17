8666923ac637807c02f56793c0003a3a4b2210c6
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-10-17 09:26:15 -0400

    server running

--- models/workout.js
+++ models/workout.js
@@ -1,17 +1,20 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter a name for workout"
-    },
-    date: {
+    day: {
         type: Date,
-        default: Date.now
-    }
+        required: "Date is required"
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true,
+                required: "Type is required"
+            }
+        }
+    ]
 });
   
 const Workout = mongoose.model("Workout", workoutSchema);
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/html");
+const Workout = require("./models/workout");
 
 const PORT = process.env.PORT || 3000;
 
@@ -15,11 +16,13 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
-app.use('/api', apiRoutes);
+// app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
