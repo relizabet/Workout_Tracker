ad2c0a1de46936c0e590135c02d840e6aeee0fe9
Rachel Campbell <rach.eliza.campbell@icloud.com>
2020-10-18 13:19:30 -0400

    added dotenv

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const mongoose = require("mongoose");
 const logger = require("morgan");
 const apiRoutes = require("./routes/api");
 const htmlRoutes = require("./routes/html");
-const Workout = require("./models/workout");
+
+require("dotenv").config();
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,7 +16,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
